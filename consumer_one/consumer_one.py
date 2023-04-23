9722b1df92a54a4b98287284a78d0733bdeafba7
Gigi6027 <97170431+Gigi6027@users.noreply.github.com>
2023-04-23 18:08:26 +0530

    Update consumer_one.py

--- consumer_one/consumer_one.py
+++ consumer_one/consumer_one.py
@@ -1,15 +1,23 @@
 import pika
+
+# Establish connection with RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.28.0.1'))
 channel = connection.channel()
 
-
+# Declare a queue named 'health_check_queue' with durable=True to ensure messages are not lost even if RabbitMQ server restarts
 channel.queue_declare(queue='health_check_queue', durable=True)
 
+# Define a callback function to handle messages received on 'health_check_queue'
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body.decode(),flush=True)
+    # Print the received message to console
+    print(" [x] Received %r" % body.decode(), flush=True)
+    # Acknowledge receipt of the message to RabbitMQ server
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+# Consume messages on 'health_check_queue' using the callback function defined above
 channel.basic_consume(queue='health_check_queue', on_message_callback=callback)
 
-print(' [*] Waiting for messages. To exit press CTRL+C',flush=True)
+# Start consuming messages indefinitely until the script is interrupted
+print(' [*] Waiting for messages. To exit press CTRL+C', flush=True)
 channel.start_consuming()
+
